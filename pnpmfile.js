1ecebf6c888b142f04aaa03c0257aef6369bbd9f
Sam Gwilym <gwilym@me.com>
2020-08-06 14:21:28 +0200

    Make this run on Glitch

---
+++ pnpmfile.js
@@ -0,0 +1,30 @@
+// This makes this package install correctly on Glitch, where PNPM is used to install dependencies
+
+const { resolutions } = JSON.parse(
+  require("fs").readFileSync("./package.json", "utf-8")
+);
+
+if (resolutions) {
+  module.exports = {
+    hooks: {
+      readPackage,
+    },
+  };
+
+  function readPackage(pkg, context) {
+    if (pkg.dependencies) {
+      for (const k in resolutions) {
+        if (pkg.dependencies[k] && pkg.dependencies[k] !== resolutions[k]) {
+          context.log(
+            `"${k}@${pkg.dependencies[k]}" overriden in "${pkg.name}" to "${k}@${resolutions[k]}"`
+          );
+          pkg.dependencies[k] = resolutions[k];
+        }
+      }
+    }
+
+    return pkg;
+  }
+} else {
+  module.exports = {};
+}
