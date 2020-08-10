bd7c980c56c34b549165a74222d1b344e5184c40
Sam Gwilym <gwilym@me.com>
2020-08-10 12:05:51 +0200

    Make pub detect existing persisted workspaces

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const { ApolloServer } = require("apollo-server");
 const path = require("path");
 const fs = require("fs");
 
+// A list of workspaces that should always be on this pub.
+// If a workspace here isn't present yet, we'll create it.
+const bootstrapWorkspaces = ["+gardening.xxxxxxxxxxxxxxxxxxxx"];
+
 // The name of the directory where you'd like to store your workspaces
 const WORKSPACE_DIR = `.data`;
 
@@ -14,13 +18,35 @@ if (!workspacesDirExists) {
   fs.mkdirSync(path.resolve(WORKSPACE_DIR));
 }
 
+// We'll use this regex to find the workspace names from SQLite files
+const sqliteRegex = /.*\/?(\+.*\.*)\.sqlite/;
+
+// Create a list of workspaces persisted as SQLite files in the .data directory
+const existingWorkspaces =
+  fs
+    .readdirSync(WORKSPACE_DIR)
+    .filter((fileName) => fileName.match(sqliteRegex))
+    .map((fileName) => {
+      const result = sqliteRegex.exec(fileName);
+
+      if (result) {
+        return result[1];
+      }
+
+      return undefined;
+    })
+    .filter((result) => !!result) || [];
+
 // Create a schema context
 // This tells earthstar-graphql how to store your workspace
 // And also lets you configure some options,
 // e.g. whether new workspaces can be added
 const context = createSchemaContext("SQLITE", {
-  // Enter the names of your workspaces here
-  workspaceAddresses: ["+gardening.xxxxxxxxxxxxxxxxxxxx"],
+  // We use a set here so that workspaces which were found in the bootstrap list
+  // and the existing persisted workspaces aren't repeated
+  workspaceAddresses: Array.from(
+    new Set([...bootstrapWorkspaces, ...existingWorkspaces])
+  ),
   // Don't change this unless you want to change where workspaces are stored
   getWorkspacePath: (addr) => path.join(WORKSPACE_DIR, `${addr}.sqlite`),
 });
